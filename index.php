757cc6ac16b32b0f1dcec7b44664196e5563b97e
milad_develper <milad@gmail.com>
2018-05-08 14:16:05 +0430

    3 commit

--- index.php
+++ index.php
@@ -5,32 +5,57 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <meta name="description" content="">
         <meta name="author" content="">
+        <title>Sefroweb Static</title>
         <link rel="icon" href="../../../../favicon.ico">
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 
+
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.12/css/all.css" integrity="sha384-G0fIWCsCzJIMAVNQPfjH08cyYaUtMwjJwqiRKxxE/rx96Uroj1BtIQ6MLJuheaO9" crossorigin="anonymous">
         <title>Floating labels example for Bootstrap</title>
 
         <!-- Bootstrap core CSS -->
         <link href="css/bootstrap.min.css" rel="stylesheet">
         <link href="css/rtl.css" rel="stylesheet">
-
+        <link href="css/saltern.css" rel="stylesheet" type="text/css"/>
+        <link href="fonts/font.css" rel="stylesheet" type="text/css"/>
         <!-- Custom styles for this template -->
         <link href="css/floating-labels.css" rel="stylesheet">
     </head>
 
     <body>
+
         <form class="form-signin">
 
-            <div class="heading">
+            <div class="container-fluid">
+                <div class="row">
 
-                <div class="rightHead">
+                    <div class="heading">
+                        <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+                            <div  class="rightHead ">
+                                <a class="fontStd" href="">
+                                    پرسش
+                                    <i class="fas fa-plus top1px"></i>
+                                </a>
+                            </div>
+                        </div>
+                        <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+                            <div class="centerHead">
+                                <!--<img src="img/sefroweb.PNG" alt=""/>-->
+                            </div>
+                        </div>
+                        <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+                            <div class="leftHead">
+                                <a class="fontStd" href="">
+                                    پرسش
+                                    <i class="fas fa-plus top1px"></i>
+                                </a>
+                            </div>
+                        </div>
+
+                    </div>
                 </div>
-
-                <div class="centerHead">
-                </div>
-
-                <div class="leftHead">
-                </div>
-
             </div>
             <div class="text-center mb-4">
                 <img class="mb-4" src="https://getbootstrap.com/assets/brand/bootstrap-solid.svg" alt="" width="72" height="72">
