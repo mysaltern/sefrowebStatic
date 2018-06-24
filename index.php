29c18e7d531dc60e0b786c89874748708eba08e4
milad_develper <milad@gmail.com>
2018-06-24 12:12:28 +0430

    hosein editing

--- index.php
+++ index.php
@@ -1,179 +1,172 @@
 <!DOCTYPE html>
 <html lang="en">
 
-    <head>
+	<head>
 
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <meta name="description" content="">
-        <meta name="author" content="">
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<meta name="description" content="">
+		<meta name="author" content="">
 
-        <title>Sefroweb</title>
+		<title>Sefroweb</title>
 
-        <!-- Bootstrap core CSS -->
-        <link href="assets/css/bootstrap.css" rel="stylesheet" type="text/css"/>
-        <link href="assets/css/rtl.css" rel="stylesheet" type="text/css"/>
-        <!-- Custom styles for this template -->
-        <link href="assets/css/mrh.css" rel="stylesheet" type="text/css"/>
-        <link href="assets/css/saltern.css" rel="stylesheet" type="text/css"/>
-        <!--<link href="assets/css/reset.css" rel="stylesheet" type="text/css"/>-->
-        <link href="assets/css/style.css" rel="stylesheet" type="text/css"/>
-        <!--<link href="assets/css/demo.css" rel="stylesheet" type="text/css"/>-->
-        <link href="assets/fonts/font.css" rel="stylesheet" type="text/css"/>
+		<!-- Bootstrap core CSS -->
+		<link href="assets/css/bootstrap-rtl.css" rel="stylesheet" type="text/css"/>
+		<!-- Custom styles for this template -->
+		<link href="assets/css/mrh.css" rel="stylesheet" type="text/css"/>
+		<link href="assets/css/saltern.css" rel="stylesheet" type="text/css"/>
+		<!--<link href="assets/css/reset.css" rel="stylesheet" type="text/css"/>-->
+		<link href="assets/css/style.css" rel="stylesheet" type="text/css"/>
+		<!--<link href="assets/css/demo.css" rel="stylesheet" type="text/css"/>-->
+		<link href="assets/fonts/font.css" rel="stylesheet" type="text/css"/>
 
-        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.12/css/all.css" integrity="sha384-G0fIWCsCzJIMAVNQPfjH08cyYaUtMwjJwqiRKxxE/rx96Uroj1BtIQ6MLJuheaO9" crossorigin="anonymous">
-        <!--<link href="assets/css/floating-labels.css" rel="stylesheet" type="text/css"/>-->
-    </head>
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.12/css/all.css" integrity="sha384-G0fIWCsCzJIMAVNQPfjH08cyYaUtMwjJwqiRKxxE/rx96Uroj1BtIQ6MLJuheaO9" crossorigin="anonymous">
+		<!--<link href="assets/css/floating-labels.css" rel="stylesheet" type="text/css"/>-->
+	</head>
 
-    <body>
-
-
-
-        <!--header-->
-        <!--header-->
-        <!--header-->
-        <!--header-->
-
-        <?php include 'header.php'; ?>
-
-
-        <!--header-->
-        <!--header-->
-        <!--header-->
-
-        <!-- Page Content -->
-        <div class="container">
-
-            <div style="overflow: auto;">
-                <ul class="nav nav-tabs" id="myTab" role="tablist" style="width: 800px;">
-                    <li class="nav-item">
-                        <a class="nav-link active" id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" id="profile-tab" data-toggle="tab" href="#profile" role="tab" aria-controls="profile" aria-selected="false">Profile</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" id="contact-tab" data-toggle="tab" href="#contact" role="tab" aria-controls="contact" aria-selected="false">Contact</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" id="contact-tab" data-toggle="tab" href="#contact2" role="tab" aria-controls="contact" aria-selected="false">Contact2</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" id="contact-tab" data-toggle="tab" href="#contact3" role="tab" aria-controls="contact" aria-selected="false">Contact3</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" id="contact-tab" data-toggle="tab" href="#contact4" role="tab" aria-controls="contact" aria-selected="false">Contact4</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" id="contact-tab" data-toggle="tab" href="#contact5" role="tab" aria-controls="contact" aria-selected="false">Contact5</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" id="contact-tab" data-toggle="tab" href="#contact6" role="tab" aria-controls="contact" aria-selected="false">Contact6</a>
-                    </li>
-                </ul>
-            </div>
-
-            <div class="tab-content" id="myTabContent">
-                <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
-
-                    <div class="container">
-                        <div class="card stdCard" style="">
-                            <div class="profileProgress card-body stdCard">
-                                <div class="progress">
-                                    <div class="progress-bar" style="width:70%"></div>
-                                </div>
-
-                                <div class="fontStd">
-                                    اطلاعات پروفایل خود را کامل کنید :
-                                </div>
-
-                                <div class="row">
-                                    <div class="fontStd col-xl-4 col-sm-12 col-md-4">
-                                        <button type="button" class="btn btn-success btn-block">پروفایل</button>
-
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-                    Looking for something more than just a free theme? Head over to Creative Tim and preview some of the best premium Bootstrap themes and templates on the web!
-
-                </div>
-                <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">Business Frontpage is a basic HTML starter template for creating a Bootstrap based website for a small business or other organization.</div>
-                <div class="tab-pane fade" id="contact" role="tabpanel" aria-labelledby="contact-tab">Really nice Template. The best one for a good lokking Landingpage! Thank you very much!
-                    But i have a question, i have a problem with changin the navigationbar's colour. I tried to colorize it in blue or green, but it doesn't work. Can anybody help me to fix that problem??</div>
-                <div class="tab-pane fade" id="contact2" role="tabpanel" aria-labelledby="contact-tab">Everything looks great, except for one thing. Where is the business-casual.css file that is mentioned in this bit of code??</div>
-                <div class="tab-pane fade" id="contact3" role="tabpanel" aria-labelledby="contact-tab">I am having issue with the navigation bar. it seems like its covering part of my content. Even I went to business-frontpage.css and change the padding-top. It still covers my About title. Anyone has the same issue?</div>
-                <div class="tab-pane fade" id="contact4" role="tabpanel" aria-labelledby="contact-tab"></div>
-                <div class="tab-pane fade" id="contact5" role="tabpanel" aria-labelledby="contact-tab"></div>
-                <div class="tab-pane fade" id="contact6" role="tabpanel" aria-labelledby="contact-tab">...</div>
-            </div>
-
-
-            <div class="row">
-                <div class="col-sm-4 my-4">
-                    <div class="card">
-                        <img class="card-img-top" src="http://placehold.it/300x200" alt="">
-                        <div class="card-body">
-                            <h4 class="card-title">Card title</h4>
-                            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente esse necessitatibus neque sequi doloribus.</p>
-                        </div>
-                        <div class="card-footer">
-                            <a href="#" class="btn btn-primary">Find Out More!</a>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-sm-4 my-4">
-                    <div class="card">
-                        <img class="card-img-top" src="http://placehold.it/300x200" alt="">
-                        <div class="card-body">
-                            <h4 class="card-title">Card title</h4>
-                            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente esse necessitatibus neque sequi doloribus totam ut praesentium aut.</p>
-                        </div>
-                        <div class="card-footer">
-                            <a href="#" class="btn btn-primary">Find Out More!</a>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-sm-4 my-4">
-                    <div class="card">
-                        <img class="card-img-top" src="http://placehold.it/300x200" alt="">
-                        <div class="card-body">
-                            <h4 class="card-title">Card title</h4>
-                            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente esse necessitatibus neque.</p>
-                        </div>
-                        <div class="card-footer">
-                            <a href="#" class="btn btn-primary">Find Out More!</a>
-                        </div>
-                    </div>
-                </div>
-
-            </div>
-            <!-- /.row -->
-
-        </div>
-
-        <hr/>
-        <hr/>
-
-
-        <hr/>
+	<body>
 
 
 
-        <!--        footer   footer        footer   footer
-                footer   footer
-        -->
+		<!--header-->
+		<!--header-->
+		<!--header-->
+		<!--header-->
 
-        <?php include 'footer.php'; ?>
-
-
-        <!--
-                footer   footer
-                footer   footer-->
+		<?php include 'header.php'; ?>
 
 
-    </body>
+		<!--header-->
+		<!--header-->
+		<!--header-->
+
+		<!-- Page Content -->
+		<div class="container">
+
+			<div style="overflow: auto;">
+				<ul class="nav nav-tabs" id="myTab" role="tablist" style="width: 800px;">
+					<li class="nav-item">
+						<a class="nav-link active" id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true">Home</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" id="profile-tab" data-toggle="tab" href="#profile" role="tab" aria-controls="profile" aria-selected="false">Profile</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" id="contact-tab" data-toggle="tab" href="#contact" role="tab" aria-controls="contact" aria-selected="false">Contact</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" id="contact-tab" data-toggle="tab" href="#contact2" role="tab" aria-controls="contact" aria-selected="false">Contact2</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" id="contact-tab" data-toggle="tab" href="#contact3" role="tab" aria-controls="contact" aria-selected="false">Contact3</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" id="contact-tab" data-toggle="tab" href="#contact4" role="tab" aria-controls="contact" aria-selected="false">Contact4</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" id="contact-tab" data-toggle="tab" href="#contact5" role="tab" aria-controls="contact" aria-selected="false">Contact5</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" id="contact-tab" data-toggle="tab" href="#contact6" role="tab" aria-controls="contact" aria-selected="false">Contact6</a>
+					</li>
+				</ul>
+			</div>
+
+			<div class="tab-content" id="myTabContent">
+				<div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
+
+					<div class="container">
+						<div class="card stdCard" style="">
+							<div class="profileProgress card-body stdCard">
+								<div class="progress">
+									<div class="progress-bar" style="width:70%"></div>
+								</div>
+
+								<div class="fontStd">
+									اطلاعات پروفایل خود را کامل کنید :
+								</div>
+
+								<div class="row">
+									<div class="fontStd col-xl-4 col-sm-12 col-md-4">
+										<button type="button" class="btn btn-success btn-block">پروفایل</button>
+
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+
+					Looking for something more than just a free theme? Head over to Creative Tim and preview some of the best premium Bootstrap themes and templates on the web!
+
+				</div>
+				<div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">Business Frontpage is a basic HTML starter template for creating a Bootstrap based website for a small business or other organization.</div>
+				<div class="tab-pane fade" id="contact" role="tabpanel" aria-labelledby="contact-tab">Really nice Template. The best one for a good lokking Landingpage! Thank you very much!
+					But i have a question, i have a problem with changin the navigationbar's colour. I tried to colorize it in blue or green, but it doesn't work. Can anybody help me to fix that problem??</div>
+				<div class="tab-pane fade" id="contact2" role="tabpanel" aria-labelledby="contact-tab">Everything looks great, except for one thing. Where is the business-casual.css file that is mentioned in this bit of code??</div>
+				<div class="tab-pane fade" id="contact3" role="tabpanel" aria-labelledby="contact-tab">I am having issue with the navigation bar. it seems like its covering part of my content. Even I went to business-frontpage.css and change the padding-top. It still covers my About title. Anyone has the same issue?</div>
+				<div class="tab-pane fade" id="contact4" role="tabpanel" aria-labelledby="contact-tab"></div>
+				<div class="tab-pane fade" id="contact5" role="tabpanel" aria-labelledby="contact-tab"></div>
+				<div class="tab-pane fade" id="contact6" role="tabpanel" aria-labelledby="contact-tab">...</div>
+			</div>
+
+
+			<div class="row">
+				<div class="col-sm-12 my-4">
+					<div class="card px-2">
+						<h2>عنوان سوال مطرح شده در سایت چیست؟</h2>
+						<div class="row">
+							<div class="col-sm-3">
+								<img class="card-img-top" src="http://placehold.it/300x200" alt="">
+								<div class="row my-1">
+									<div class="col-4 align-self-start">
+										<i class="fas fa-thumbs-up float-right">111</i>
+									</div>
+									<div class="col-4 align-self-center">
+										<i class="fas fa-eye float-right">500</i>
+									</div>
+									<div class="col-4 align-self-end">
+										<i class="fas fa-star float-right">12</i>
+									</div>
+
+								</div>
+
+
+							</div>
+							<div class="col-sm-9">
+								<p> توضیح مختصری در رابطه با سوال. البته به نظر میرسه همینجا میشه سرو تهش رو هم آورد اگه سوال زیاد طولانی نباشه و چیز اضافه ای نداشته باشه</p>
+								<span class="rtl float-left">5 روز مانده تا انقضای سوال</span></br>
+								<button type="button" class="btn btn-outline-primary">نظر</button>
+								<button type="button" class="btn btn-outline-secondary">پاسخ</button>
+							</div>
+						</div>
+					</div>
+				</div>
+
+			</div>
+			<!-- /.row -->
+
+		</div>
+
+		<hr/>
+		<hr/>
+
+
+		<hr/>
+
+
+
+		<!--        footer   footer        footer   footer
+						footer   footer
+		-->
+
+		<?php include 'footer.php'; ?>
+
+
+		<!--
+						footer   footer
+						footer   footer-->
+
+
+	</body>
 
 </html>
